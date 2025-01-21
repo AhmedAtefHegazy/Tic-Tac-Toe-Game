21287af60b859c47fe0e17e5acbd994a7a7fa1a2
ahmedatefsyhh <ahmedatef10987@gmail.com>
2025-01-21 17:20:49 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Tic_Tac_Toe_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
